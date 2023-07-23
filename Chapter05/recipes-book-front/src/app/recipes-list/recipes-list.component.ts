085ef02767d342a1a2fdeab2a7b3ad1475c17aa6
Tarik <tarikkunduklija@gmail.com>
2023-07-23 13:48:46 -0700

    updating with strategy checks

--- Chapter05/recipes-book-front/src/app/recipes-list/recipes-list.component.ts
+++ Chapter05/recipes-book-front/src/app/recipes-list/recipes-list.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RecipesService } from '../core/services/recipes.service';
+import { Observable, catchError, from, map, of, retry, retryWhen, tap, throwError } from 'rxjs';
+import { Recipe } from '../core/model/recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -9,13 +11,95 @@ import { RecipesService } from '../core/services/recipes.service';
 })
 export class RecipesListComponent implements OnInit {
 
-  recipes$ = this.service.recipes$;
-
   constructor(private service: RecipesService) { }
-
+  public recipes$!: Observable<Recipe[]>;
+  
   ngOnInit(): void {
+    // this.replaceStrategy()
+    // this.rethrowStrategy()
+    // this.retryStrategy()
+    // this.retryWhenStrategy();
+    this.recipes$ = this.service.recipes$;
   }
 
+  replaceStrategy(){
+    const stream$ = from(['5', '10', '6', 'Hello', '2']);
+    stream$.pipe(map((value) => {
+      if (isNaN(value as any)) {
+        throw new Error('This is not a number')
+      }
+      return parseInt(value)
+    }),
+      catchError((error) => {
+        console.log('Caught Error', error);
+        return of();
+      })
+    ).subscribe({
+      next: (res) => console.log('Value Emitted', res),
+      error: (err) => console.log('Error Occurred', err),
+      complete: () => console.log('Stream Completed'),
+    })
+  }
 
+  rethrowStrategy(){
+    const stream$ = from(['5', '10', '6', 'Hello', '2']);
+    stream$.pipe(map((value) => {
+      if(isNaN(value as any)) {
+        throw new Error('This is not a number!');
+      }
+      return parseInt(value);
+    }),
+      catchError((error) => {
+        console.log('Caught Error', error);
+        return throwError(() => error)
+      })
+    ).subscribe({
+      next: (res) => console.log('Value emitted', res),
+      error: (err) => console.log('Error Occurred', err),
+      complete: () => console.log('Stream Completed')
+    })
+  }
 
+  retryStrategy(){
+    const stream$ = from(['5', '10', '6', 'Hello', '2']);
+    stream$.pipe(map((value) => {
+      if(isNaN(value as any)) {
+        throw new Error('This is not a number!');
+      }
+      return parseInt(value);
+    }),
+      retry(2),
+      catchError((error) => {
+        console.log('Caught Error', error);
+        return throwError(() => error)
+      })
+    ).subscribe({
+      next: (res) => console.log('Value Emitted', res),
+      error: (err) => console.log('Error Occurred', err),
+      complete: () => console.log('Stream Completed')
+    })
+  }
+
+  retryWhenStrategy(){
+    const stream$ = from(['5', '10', '6', 'Hello', '2']);
+    stream$.pipe(map((value) => {
+      if(isNaN(value as any)) {
+        throw new Error('This is not a number!');
+      }
+      return parseInt(value);
+    }),
+      retryWhen((errors) => {
+        return errors.pipe(
+          tap(() => console.log('Retrying the source Observable...'))
+        )
+      })
+    ).subscribe({
+      next: (res) => console.log('Value Emitted', res),
+      error: (err) => console.log('Error Occurred', err),
+      complete: () => console.log('Stream Completed')
+    })
+  }
+
+  delayWhenStrategy(){
+  }
 }
